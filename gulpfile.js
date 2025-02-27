645a7d688aaa66cafb21548c6883e9f78317b25f
Nikoloz Pairazian <nika.pairazian@gmail.com>
2025-02-27 18:09:05 +0400

    deploy version

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,44 @@
-"use strict";
+import gulp from "gulp";
+import sass from "gulp-sass";
+import * as sassCompiler from "sass";
+import cleanCSS from "gulp-clean-css";
+import rename from "gulp-rename";
+import uglify from "gulp-uglify";
 
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const cleanCSS = require("gulp-clean-css"); 
-const rename = require("gulp-rename");
+const sassTask = sass(sassCompiler);
 
-exports.sass = function () {
+export function styles() {
     return gulp
         .src("./src/styles/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(cleanCSS()) 
+        .pipe(sassTask().on("error", sassTask.logError))
+        .pipe(cleanCSS())
         .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("./dist"));
-};
+        .pipe(gulp.dest("./dist/styles"));
+}
 
-exports.watch = function () {
-    gulp.watch("./src/styles/*.scss", gulp.series(exports.sass));
-};
+export function scripts() {
+    return gulp
+        .src("./src/js/*.js")
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("./dist/js"));
+}
+
+export function html() {
+    return gulp.src("./*.html").pipe(gulp.dest("./dist"));
+}
+
+export function images() {
+    return gulp.src("./src/images/**/*").pipe(gulp.dest("./dist/images"));
+}
+
+export function watch() {
+    gulp.watch("./src/styles/*.scss", styles);
+    gulp.watch("./src/js/*.js", scripts);
+    gulp.watch("./src/*.html", html);
+    gulp.watch("./src/images/**/*", images);
+}
+
+export const build = gulp.series(gulp.parallel(styles, scripts, html, images));
+
+export default watch;
